ac65681cae3e06b7e058dce58d06267eeb2d8383
Bryce Allen <bdallen@uchicago.edu>
2022-08-04 16:12:56 -0500

    build with -O1 to compare, print PASS at end

--- run.sh
+++ run.sh
@@ -2,9 +2,20 @@
 
 set -eu
 
-CXX=$(which hipcc) cmake -S . -B  build-hip \
+ARCHS=${HIP_GPU_ARCHITECTURES:=gfx906a:xnack+}
+
+CXX=$(which hipcc) cmake -S . -B  build-hip-o2 \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCXX_FLAGS_RELWITHDEBINFO="-g -O2 -NDEBUG -DGT_BOUNDSCHECK" \
-  -DHIP_GPU_ARCHITECTURES=gfx1030
-cmake --build build-hip -v
-./build-hip/gtensor_unravel_reproducer
+  -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-g -O2 -DNDEBUG" \
+  -DHIP_GPU_ARCHITECTURES=$ARCHS
+cmake --build build-hip-o2 -v
+CXX=$(which hipcc) cmake -S . -B  build-hip-o1 \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-g -O1 -DNDEBUG" \
+  -DHIP_GPU_ARCHITECTURES=$ARCHS
+cmake --build build-hip-o1 -v
+
+echo "Running -O1 build..."
+./build-hip-o1/gtensor_unravel_reproducer
+echo "Running -O2 build..."
+./build-hip-o2/gtensor_unravel_reproducer
--- src/gtensor_unravel_reproducer.cxx
+++ src/gtensor_unravel_reproducer.cxx
@@ -1,12 +1,19 @@
+#include <iostream>
+
+#define GT_BOUNDSCHECK
 #include "gtensor/gtensor.h"
 
 int main(int argc, char *argv[]) {
   auto rhs = gt::empty_device<double>({256, 16, 24, 24, 24, 24});
   auto k_rhs = rhs.to_kernel();
+  // NOTE: with the default option of GTENSOR_PER_DIM_KERNELS=OFF, this will do
+  // a 1d kernel launch an "unravel" the 1d value into 6d to pass in to the
+  // passed functor
   gt::launch<6>(
       rhs.shape(), GT_LAMBDA(int i, int j, int k, int l, int m, int n) {
         k_rhs(i, j, k, l, m, n) = 5.;
       });
   gt::synchronize();
+  std::cout << "PASS" << std::endl;
   return 0;
 }
